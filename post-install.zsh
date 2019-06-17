18385e93637d38c6a820f5bc4f8867093c024f7b
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-06-17 12:48:08 -0300

    Set Hyper as the default terminal of GNOME

--- post-install.zsh
+++ post-install.zsh
@@ -3,3 +3,6 @@
 # Install nvm node version
 source ~/.nvm/nvm.sh
 nvm install v10.16.0
+
+# Set Hyper as the default terminal of GNOME
+gsettings set org.gnome.desktop.default-applications.terminal exec 'hyper'
