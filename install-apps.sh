96fa05ab9db6ca6639555fcf0a8ae5531be2bab4
Rafael Almeida <rafaelalmeidatk@gmail.com>
2019-07-02 12:30:20 -0300

    Update repositories before installing yarn

--- install-apps.sh
+++ install-apps.sh
@@ -33,6 +33,7 @@ if ! [ -x "$(command -v yarn)" ]; then
     echo 'Installing Yarn...'
     sh -c "curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -"
     sh -c "echo \"deb https://dl.yarnpkg.com/debian/ stable main\" | sudo tee /etc/apt/sources.list.d/yarn.list"
+    sudo apt-get update
     sudo apt-get install -y --no-install-recommends yarn
 fi
 
